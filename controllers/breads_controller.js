1b3ee218285b9e43caf4299d7a8b0e74517fd561
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-07-19 19:48:49 -0400

    sending index and show data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,8 +2,14 @@ const express = require('express');
 const breads = express.Router();
 const Bread = require('../models/breads');
 
-breads.get('/:arrayIndex', (req, res) => {
-    res.send(Bread[req.params.arrayIndex]);
+breads.get('/', (req, res) => {
+    res.render('Index',
+        {
+            breads: Bread,
+            title: 'Index Page'
+        }
+    )
+    // res.send(Bread);
 });
 
 module.exports = breads;
