c6f0ef5b943ece1b33cc7d63e8beede04388eca8
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-07-24 12:36:46 -0400

    edit and update routes complete

--- server.js
+++ server.js
@@ -1,14 +1,20 @@
+// DEPENDENCIES
 const express = require('express');
+const methodOverride = require('method-override')
+
 
 require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
 
+// MIDDLEWARE
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
 
 app.get('/', (req, res) => {
     res.send('Welcome to an Awesome App about Breads!');
@@ -17,7 +23,7 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controller');
 app.use('/breads', breadsController);
 
-app.get('*', (req,res) => {
+app.get('*', (req, res) => {
     res.send('404');
 })
 
--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react');
 const Default = require('./layouts/default');
 
-const Show = ({bread}) => {
+const Show = ({ bread, index }) => {
     console.log(bread.name);
     return (
         <Default>
@@ -11,13 +11,21 @@ const Show = ({bread}) => {
                 and it
                 {
                     bread.hasGluten
-                    ? <span> does </span>
-                    : <span> does NOT </span>
+                        ? <span> does </span>
+                        : <span> does NOT </span>
                 }
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name} />
             <li><a href="/breads">Go home</a></li>
+
+            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+
+
+            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+                <input type='submit' value="DELETE" />
+            </form>
+
         </Default>
     )
 }
