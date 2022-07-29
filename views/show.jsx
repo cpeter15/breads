54087b7a1363e1c7a1fc005a944c8d75028d8208
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-07-28 20:15:45 -0400

    added mongoose

--- views/show.jsx
+++ views/show.jsx
@@ -21,7 +21,6 @@ const Show = ({ bread, index }) => {
 
             <a href={`/breads/${index}/edit`}><button>Edit</button></a>
 
-
             <form action={`/breads/${index}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE" />
             </form>
