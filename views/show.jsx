34740c5615f28a249050b49dc175753613770a53
cpeter15 <caleb.j.peterson9687@gmail.com>
2022-07-31 11:32:49 -0400

    added validation

--- views/show.jsx
+++ views/show.jsx
@@ -17,13 +17,14 @@ const Show = ({ bread, index }) => {
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name} />
-            <li><a href="/breads">Go home</a></li>
+            <p>{bread.getBakedBy()}</p>
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
 
-            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
-
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+            <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
                 <input type='submit' value="DELETE" />
             </form>
+
+            <li><a href="/breads">Go home</a></li>
 
         </Default>
     )
